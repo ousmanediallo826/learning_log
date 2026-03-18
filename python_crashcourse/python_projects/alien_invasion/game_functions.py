79dff650213897f34d1c1019795f40e24d92d412
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2026-03-17 22:58:45 -0400

    save changes before untracking

--- python_crashcourse/python_projects/alien_invasion/game_functions.py
+++ python_crashcourse/python_projects/alien_invasion/game_functions.py
@@ -2,23 +2,23 @@ import sys
 
 import pygame
 
-from python_crashcourse.python_basic.python_projects.alien_invasion import ship
+from ship import Ship
 
 
-def check_events():
+def check_events(ship):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             check_keydown_events(event, ship)
-        elif event.key == pygame.K_UP:
+        elif event.type == pygame.K_UP:
             check_keyup_events(event, ship)
 
 
 
 
-def update_screen(ai_settings, screen, ship, aliens):
+def update_screen(ai_settings, screen, ship):
     """Update the screen with the current state."""
     screen.fill(ai_settings.bg_color)
     ship.blitme()
